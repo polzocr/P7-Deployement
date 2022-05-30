0f7844a0961d175b27a2e2b2e2001def723026e1
Polzocr <micro.cpt.62@outlook.fr>
2022-05-30 22:30:09 +0200

    azda

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'https://polzocr.github.io/P7-Deployement/',
+  publicPath: process.env.NODE_ENV === "production" ? "/P7-Deployement/" : "/",
   transpileDependencies: true,
   css:{
     loaderOptions: {
